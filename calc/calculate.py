27b059a7a7eeb86c2d784af016b36a6909206411
minseon6371 <alstjs4739@kookmin.ac.kr>
2020-06-24 01:35:11 +0900

    issue #3 start version control calculate.conf,calculate.py,template1.py

---
+++ calc/calculate.py
@@ -0,0 +1,21 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+  
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
+    if '' not in [a, b]:
+ 	a, b = int(a), int(b)
+    response_body = html % {
+	'sum' : a+b,
+	'mul' : a*b, 
+    } 
+    start_response('200 OK', [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
+
+
---
+++ calc/template1.py
@@ -0,0 +1,15 @@
+html = """
+<html>
+    <body>
+        <form method="GET" action="">
+            num1 = <input type="number" name="a"> 
+            num2 = <input type="number" name="b"></br>
+            <input type="submit">
+        </form>
+        <p>
+	sum : %(sum)s</br>
+	mul : %(mul)s</br>
+	</p> 
+    </body>
+</html>
+"""
